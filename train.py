2cbf4120b519fae74accb0de6dfd9fc6eaeb6732
Justus Languell <jlanguell@trainbeyond.com>
2023-12-28 12:08:18 -0600

    trained on 3070

--- train.py
+++ train.py
@@ -3,5 +3,5 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.pt")
 
 if __name__ == '__main__':
-    results = model.train(data="datasets/data.yaml", epochs=500, device='mps')
+    results = model.train(data="datasets/data.yaml", epochs=500, device=0)
     results = model.val()
